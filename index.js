1f2b814623ced3f08071886675c85e238dc1caba
Sean Carter <scarter0313@gmail.com>
2019-01-03 04:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+
+function happyHolidays(string) {
+    return "Happy holidays!"
+}
+
+function happyHolidays("Happy holidays, ${name}!) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
